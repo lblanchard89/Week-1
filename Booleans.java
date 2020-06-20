de23fcdcbe87073577acfaea8e5b30ba217a1c3c
Lee <lblanchard89@gmail.com>
2020-06-19 21:21:46 -0500

    uploading week 2 project

--- Booleans.java
+++ Booleans.java
@@ -1,17 +1,15 @@
 
 public class Booleans {
-
 	public static void main(String[] args) {
-	boolean isHotOutside = true;
-	boolean isWeekday = true;
-	boolean hasMoneyInPocket = false;
-	double costOfMilk = 2.75;
-	double moneyInWallet = 87.65;
-	int thirstLevel = 9;
-	boolean shouldByIcecream = isHotOutside && hasMoneyInPocket;
-	boolean willGoSwimming = isHotOutside && !isWeekday;
-	boolean isAGoodDay = isHotOutside && hasMoneyInPocket && !isWeekday;
-	boolean willBuyMilk = isHotOutside && (thirstLevel >= 3) && (moneyInWallet >= (costOfMilk * 2));
-	}
-
+		boolean isHotOutside = true;
+		boolean isWeekday = true;
+		boolean hasMoneyInPocket = false;
+		double costOfMilk = 2.75;
+		double moneyInWallet = 87.65;
+		int thirstLevel = 9;
+		boolean shouldByIcecream = isHotOutside && hasMoneyInPocket;
+		boolean willGoSwimming = isHotOutside && !isWeekday;
+		boolean isAGoodDay = isHotOutside && hasMoneyInPocket && !isWeekday;
+		boolean willBuyMilk = isHotOutside && (thirstLevel >= 3) && (moneyInWallet >= (costOfMilk * 2));
+		}
 }
